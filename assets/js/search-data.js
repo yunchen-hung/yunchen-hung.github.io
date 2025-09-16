609bc1f7cf119dcab1f3ba04a7d21d665f196e3b
yunchen-hung <123218334+yunchen-hung@users.noreply.github.com>
2025-09-16 19:08:43 +0000

    Deploying to gh-pages from @ yunchen-hung/yunchen-hung.github.io@2be5d200cb15d0393b92e6a57fe26d33bae5a47f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "See my CV in the page icon!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
